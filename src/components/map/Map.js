4df6c9e850122844a43bfcc048867ac86ae328fd
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-04-04 19:05:57 -0700

    markers arent render

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-
+import { data } from '../../services/oregonData';
 
 const MapBox = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoibGFuY2VvbSIsImEiOiJjanUzMnJ4OW0wajEzNDNxdnh2ZG5ocXR6In0.3EFZBsL6gGw9kFVNbRPmTQ'
@@ -8,7 +8,14 @@ const MapBox = ReactMapboxGl({
 
 class Map extends PureComponent {
 
+
   render() {
+    const listOfMarkers = data.map(obj => {
+      return (
+        <Feature key={obj.specfic_location} coordinates={obj.coordinates}/>
+      );
+    });
+    console.log(listOfMarkers);
     return (
       <MapBox
         style="mapbox://styles/mapbox/outdoors-v10"
@@ -23,6 +30,7 @@ class Map extends PureComponent {
           id="marker"
           layout={{ 'icon-image': 'marker-15' }}>
           <Feature coordinates={[-122.634903, 45.530107]}/>
+          {listOfMarkers}
         </Layer>
       </MapBox>
     );

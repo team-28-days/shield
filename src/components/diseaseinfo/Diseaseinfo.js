eb966d7ccbc39c6c164a501d66f04207f0cc4c21
Leah White <leahawhite@gmail.com>
2019-04-05 11:14:02 -0700

    added CSS

--- src/components/diseaseinfo/Diseaseinfo.js
+++ src/components/diseaseinfo/Diseaseinfo.js
@@ -1,29 +1,28 @@
 import React from 'react';
+import styles from './Diseaseinfo.css';
 
-const Diseaseinfo = () => (
-  <div>
-    <h2>Disease Info</h2>
-    <h3>What is measles?</h3>
-    <p>Measles is a serious respiratory disease that causes a rash and fever. 
-        Measles is especially dangerous, and even deadly, for infants, 
-        small children, and individuals with weakened immune systems.</p>
-    <h3>Symptoms of measles</h3>
-    <p>Measles symptoms typically begin with high fever, cough, runny nose,
-         and red, watery eyes. After 3-5 days, a rash usually begins on the face
-          and spreads to other parts of the body.</p>
-    <h3>How is it spread?</h3> 
-    <p>Measles is extremely contagious and spreads when a person
-       with measles breathes out, coughs or sneezes. 
-       Measles travels through the air, so you can get measles 
-       just by being in a room where a person with measles has been. 
-       The virus can linger in enclosed spaces for up to two hours.</p>
-    <ul>Measles outbreaks can occur when:
-      <li>Travelers contract measles abroad and bring it back home</li>
-      <li>The virus spreads through communities 
-        with greater numbers of unvaccinated people</li>
-    </ul>
-    <p>Source: Centers for Disease Control</p>
-  </div>
-);
+function Diseaseinfo() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.disheader}>Disease Information</h2>
+      <h3 className={styles.dissubheader}>What is measles?</h3>
+      <p className={styles.discontent}>Measles is a serious respiratory disease that causes a rash and fever. 
+          Measles is especially dangerous, and even deadly, for infants, 
+          small children, and individuals with weakened immune systems.</p>
+      <h3 className={styles.dissubheader}>Symptoms of measles</h3>
+      <p className={styles.discontent}>Measles symptoms typically begin with high fever, cough, runny nose,
+          and red, watery eyes. After 3-5 days, a rash usually begins on the face
+            and spreads to other parts of the body.</p>
+      <h3 className={styles.dissubheader}>How is it spread?</h3> 
+      <p className={styles.discontent}>Measles is extremely contagious and spreads when a person
+        with measles breathes out, coughs or sneezes. 
+        Measles travels through the air, so you can get measles 
+        just by being in a room where a person with measles has been. 
+        The virus can linger in enclosed spaces for up to two hours.</p>
+      <p className={styles.discontent}>Measles outbreaks can occur when travelers contract measles abroad and bring it back home. The virus also spreads through communities with greater numbers of unvaccinated people.</p>
+      <p className={styles.discontent}>Source: Centers for Disease Control</p>
+    </div>
+  );
+}
 
 export default Diseaseinfo;
